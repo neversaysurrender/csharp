4d430669982b56533d645250d9092fa16995d4f2
neversaysurrender <1104569964@qq.com>
2019-09-19 09:35:32 +0800

    add login function to unlock screen

---
+++ lockscreen/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace lockscreen
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.unlockBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.adminNameBox = new System.Windows.Forms.TextBox();
+            this.adminLabel = new System.Windows.Forms.Label();
+            this.pwordLabel = new System.Windows.Forms.Label();
+            this.passwdBox = new System.Windows.Forms.TextBox();
+            this.restTimeLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameNotNullLabel = new System.Windows.Forms.Label();
+            this.passwdNotNullLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // unlockBtn
+            // 
+            this.unlockBtn.BackColor = System.Drawing.Color.Transparent;
+            this.unlockBtn.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.unlockBtn.Location = new System.Drawing.Point(304, 290);
+            this.unlockBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.unlockBtn.Name = "unlockBtn";
+            this.unlockBtn.Size = new System.Drawing.Size(98, 44);
+            this.unlockBtn.TabIndex = 0;
+            this.unlockBtn.Text = "解锁";
+            this.unlockBtn.UseVisualStyleBackColor = false;
+            this.unlockBtn.Click += new System.EventHandler(this.unlockBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // adminNameBox
+            // 
+            this.adminNameBox.Location = new System.Drawing.Point(291, 120);
+            this.adminNameBox.Name = "adminNameBox";
+            this.adminNameBox.Size = new System.Drawing.Size(211, 25);
+            this.adminNameBox.TabIndex = 2;
+            // 
+            // adminLabel
+            // 
+            this.adminLabel.AutoSize = true;
+            this.adminLabel.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.adminLabel.Location = new System.Drawing.Point(170, 120);
+            this.adminLabel.Name = "adminLabel";
+            this.adminLabel.Size = new System.Drawing.Size(82, 24);
+            this.adminLabel.TabIndex = 3;
+            this.adminLabel.Text = "管理员";
+            // 
+            // pwordLabel
+            // 
+            this.pwordLabel.AutoSize = true;
+            this.pwordLabel.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.pwordLabel.Location = new System.Drawing.Point(170, 213);
+            this.pwordLabel.Name = "pwordLabel";
+            this.pwordLabel.Size = new System.Drawing.Size(58, 24);
+            this.pwordLabel.TabIndex = 5;
+            this.pwordLabel.Text = "密码";
+            // 
+            // passwdBox
+            // 
+            this.passwdBox.Location = new System.Drawing.Point(291, 212);
+            this.passwdBox.Name = "passwdBox";
+            this.passwdBox.PasswordChar = '*';
+            this.passwdBox.Size = new System.Drawing.Size(211, 25);
+            this.passwdBox.TabIndex = 4;
+            // 
+            // restTimeLabel
+            // 
+            this.restTimeLabel.AutoSize = true;
+            this.restTimeLabel.Font = new System.Drawing.Font("楷体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.restTimeLabel.Location = new System.Drawing.Point(233, 386);
+            this.restTimeLabel.Name = "restTimeLabel";
+            this.restTimeLabel.Size = new System.Drawing.Size(259, 19);
+            this.restTimeLabel.TabIndex = 6;
+            this.restTimeLabel.Text = "用户名或密码错误，剩余5次";
+            this.restTimeLabel.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(555, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "取消关机";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // nameNotNullLabel
+            // 
+            this.nameNotNullLabel.AutoSize = true;
+            this.nameNotNullLabel.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nameNotNullLabel.ForeColor = System.Drawing.Color.Red;
+            this.nameNotNullLabel.Location = new System.Drawing.Point(526, 121);
+            this.nameNotNullLabel.Name = "nameNotNullLabel";
+            this.nameNotNullLabel.Size = new System.Drawing.Size(152, 19);
+            this.nameNotNullLabel.TabIndex = 9;
+            this.nameNotNullLabel.Text = "*用户名不能为空";
+            this.nameNotNullLabel.Visible = false;
+            // 
+            // passwdNotNullLabel
+            // 
+            this.passwdNotNullLabel.AutoSize = true;
+            this.passwdNotNullLabel.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.passwdNotNullLabel.ForeColor = System.Drawing.Color.Red;
+            this.passwdNotNullLabel.Location = new System.Drawing.Point(526, 213);
+            this.passwdNotNullLabel.Name = "passwdNotNullLabel";
+            this.passwdNotNullLabel.Size = new System.Drawing.Size(133, 19);
+            this.passwdNotNullLabel.TabIndex = 10;
+            this.passwdNotNullLabel.Text = "*密码不能为空";
+            this.passwdNotNullLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(249)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(707, 438);
+            this.Controls.Add(this.passwdNotNullLabel);
+            this.Controls.Add(this.nameNotNullLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.restTimeLabel);
+            this.Controls.Add(this.pwordLabel);
+            this.Controls.Add(this.passwdBox);
+            this.Controls.Add(this.adminLabel);
+            this.Controls.Add(this.adminNameBox);
+            this.Controls.Add(this.unlockBtn);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LockScreen";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button unlockBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox adminNameBox;
+        private System.Windows.Forms.Label adminLabel;
+        private System.Windows.Forms.Label pwordLabel;
+        private System.Windows.Forms.TextBox passwdBox;
+        private System.Windows.Forms.Label restTimeLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label nameNotNullLabel;
+        private System.Windows.Forms.Label passwdNotNullLabel;
+    }
+}
+
---
+++ lockscreen/MainForm.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace lockscreen
+{
+    public partial class MainForm : Form
+    {
+        #region 监听回车键
+        private const int WM_HOTKEY = 0x312; //窗口消息-热键  
+        private const int WM_CREATE = 0x1; //窗口消息-创建  
+        private const int WM_DESTROY = 0x2; //窗口消息-销毁  
+        private const int Space = 0x3572; //热键ID
+        private const int Space2 = 0x1111; //热键ID  2
+        private const int Space3 = 0x1112; //热键ID  3
+        private const int Space4 = 0x1113; //热键ID  4
+        private const int WM_CLOSE = 0x0010;    //关闭窗口消息
+        private int restTime = 6; //剩余输入密码次数
+        private bool isWaitShut = false;    //密码输错超过五次后等待关机，是否在等待状态。
+        private const int shutTime = 30;    //登陆错误5次后，电脑30秒后关机
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) //禁止关闭窗口的方法之一，将该方法加入到FormClosing事件中。
+        {
+            e.Cancel = true;
+        }
+        protected override void WndProc(ref Message m)
+        {
+            //if (m.Msg == WM_CLOSE)  //禁止alt+f4和点击X关闭窗口
+            //{
+            //    return;
+            //}
+            if (m.Msg == 0x00A1 && m.WParam.ToInt32() == 2) //禁止窗体移动
+                return;
+            
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+                case WM_HOTKEY: //窗口消息-热键ID  
+                    switch (m.WParam.ToInt32())
+                    {
+                        case Space: //热键
+                            Console.WriteLine("全局监听");
+                            break;
+                        case Space2: //热键
+                            Console.WriteLine("全局监听最小化");
+                            break;
+                        case Space3:
+                            Console.WriteLine("回来吧");
+                            this.Show();
+                            break;
+                        case Space4:
+                            Console.WriteLine("关闭吧");
+                            FormClosing -= Form1_FormClosing;   //将监听事件去除，然后关闭
+                            this.Close();
+                            break;  
+                        default:
+                            break;
+                    }
+                    break;
+                case WM_CREATE: //窗口消息-创建  
+                    AppHotKey.RegKey(Handle, Space, AppHotKey.KeyModifiers.None, Keys.Enter);
+                    AppHotKey.RegKey(Handle, Space2, AppHotKey.KeyModifiers.Alt, Keys.F4);
+                    AppHotKey.RegKey(Handle, Space3, AppHotKey.KeyModifiers.Alt | AppHotKey.KeyModifiers.Ctrl | AppHotKey.KeyModifiers.Shift, Keys.H);
+                    AppHotKey.RegKey(Handle, Space4, AppHotKey.KeyModifiers.Alt | AppHotKey.KeyModifiers.Ctrl,Keys.S);
+                    break;
+                case WM_DESTROY: //窗口消息-销毁  
+                    AppHotKey.UnRegKey(Handle, Space); //销毁热键
+                    AppHotKey.UnRegKey(Handle, Space2); //销毁热键
+                    AppHotKey.UnRegKey(Handle, Space3);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+        bool isLock = false;        //是否已锁
+        Rectangle rec_chuangkou;    //活动区域
+        Rectangle rec_outBonus;     //判断边界的区域
+        public MainForm()
+        {
+            InitializeComponent();
+            rec_chuangkou = new Rectangle(PointToScreen(new Point(1, -SystemInformation.CaptionHeight + 29)), new Size(this.ClientRectangle.Width-2, this.ClientRectangle.Height + SystemInformation.CaptionHeight - 31));
+            rec_outBonus =new Rectangle(PointToScreen(new Point(0, -SystemInformation.CaptionHeight + 28)), new Size(this.ClientRectangle.Width, this.ClientRectangle.Height + SystemInformation.CaptionHeight - 24));
+            FormClosing += Form1_FormClosing;   //监听关闭事件
+
+            isLock = true;  //一运行就上锁
+            AppHotKey.RegKey(Handle, Space, AppHotKey.KeyModifiers.None, Keys.Enter);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//当锁定时，使锁屏窗口处于最前
+        {
+            if (!this.Focused && isLock)
+            {
+                this.Activate();
+            }
+
+            if (isLock)//窗体内移动
+            {
+                //Rectangle rectangle = new Rectangle(PointToScreen(new Point(0, -SystemInformation.CaptionHeight)), new Size(this.ClientRectangle.Width, this.ClientRectangle.Height + SystemInformation.CaptionHeight));
+                if (!rec_outBonus.Contains(MousePosition))
+                    Cursor.Position = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2, Screen.PrimaryScreen.WorkingArea.Height / 2);
+            }
+
+            if (isLock)//窗体内移动
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lockBtn_Click(object sender, EventArgs e)//加锁，禁用回车键
+        {
+            isLock = true;
+            AppHotKey.RegKey(Handle, Space, AppHotKey.KeyModifiers.None, Keys.Enter);
+            
+        }
+
+        /// <summary>
+        /// 登陆管理员账号并解锁
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void unlockBtn_Click(object sender, EventArgs e)//解锁，启用回车键
+        {
+            if (isWaitShut) //如果已经输入错误超过6次，则处于等待关机状态，直接返回
+            {
+                return;
+            }
+            
+            if (this.adminNameBox.Text.Equals("") || this.passwdBox.Equals("")) //如果密码和用户名为空，则提示
+            {
+                this.nameNotNullLabel.Visible = true;
+                this.passwdNotNullLabel.Visible = true;
+                ThreadPool.QueueUserWorkItem(new WaitCallback(visibleTime =>
+                {
+                    DateTime startTime = DateTime.Now;
+                    while ((DateTime.Now - startTime).TotalSeconds < (int)visibleTime)
+                    {
+                    }
+                    Action updateLabel = () =>
+                    {
+                        this.nameNotNullLabel.Visible = false;
+                        this.passwdNotNullLabel.Visible = false;
+                    };
+                    this.Invoke(updateLabel);
+                }), 5);
+                return;
+            }
+            Console.WriteLine("name="+this.adminNameBox.Text+"pass="+this.passwdBox.Text);
+
+            restTime--; //输入的剩余密码次数减去1；
+            if (this.adminNameBox.Text.Equals("admin") && this.passwdBox.Text.Equals("123456"))
+            {
+                isLock = false;
+                AppHotKey.UnRegKey(Handle, Space);
+                this.Hide();
+            }
+            else
+            {
+                if (restTime == 0)  //剩余次数为0时，等待关机
+                {
+                    restTime = 6;
+                    isWaitShut = true;
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(shutdownTime =>
+                    {
+                        DateTime nowTime = DateTime.Now;
+                        while ((DateTime.Now - nowTime).TotalSeconds < (int)shutdownTime)
+                        {
+                            Action updateShutTipLabel = () =>
+                            {
+                                this.restTimeLabel.Text = "密码输入错误超过5次，距离关机还剩" + (30 - (int)(DateTime.Now - nowTime).TotalSeconds) + "秒";
+                            };
+                            this.restTimeLabel.Invoke(updateShutTipLabel);
+                            if (!isWaitShut)    //如果点击取消关机，则进入取消关机方法
+                            {
+                                updateShutTipLabel = () =>
+                                {
+                                    this.restTimeLabel.Text = "关机已停止";
+                                };
+                                this.Invoke(updateShutTipLabel);
+                                Thread.Sleep(5000);
+                                updateShutTipLabel = () =>
+                                {
+                                    this.restTimeLabel.Visible = false;
+                                };
+                                this.Invoke(updateShutTipLabel);
+                                return;
+                            } 
+                            Thread.Sleep(1000);
+                        }
+                        Process.Start("shutdown.exe", "-s -f");
+                    }), shutTime);
+                    return;
+                }
+                
+                //int len = this.restTimeLabel.Text.Length;   //获取剩余输入次数label的长度
+                //this.restTimeLabel.Text=this.restTimeLabel.Text.Remove(len - 2,1).Insert(len-2,restTime.ToString());    //修改剩余次数的数字
+
+                this.restTimeLabel.Text = "用户名或密码错误，剩余" + restTime.ToString() + "次";
+                if (!this.restTimeLabel.Visible)    //最开始剩余次数的label是不可见的，这时设置为可见
+                {
+                    this.restTimeLabel.Visible = true;
+                }
+                return;
+            } 
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)//鼠标的活动范围
+        {
+            if (isLock)//窗体内移动
+            {
+                Cursor.Clip = rec_chuangkou;
+            }
+            else//在全屏范围内移动
+            {
+                Cursor.Clip = Rectangle.Empty; ;
+            }
+            base.OnMouseMove(e);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            isWaitShut = false;
+        }
+
+     
+    }
+}
